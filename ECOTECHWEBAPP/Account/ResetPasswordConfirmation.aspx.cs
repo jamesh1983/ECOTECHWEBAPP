0b9b239b71ba53a41a53dea2f68ac12eebb861a7
jamesh1983 <jamesh2@163.com>
2021-01-25 15:02:44 +0800

    添加项目文件。

---
+++ ECOTECHWEBAPP/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace ECOTECHWEBAPP.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ ECOTECHWEBAPP/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ECOTECHWEBAPP.Startup))]
+namespace ECOTECHWEBAPP
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
